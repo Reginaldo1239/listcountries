dcbae51905e75b84489b3a52030e1a09922ea0fd
reginaldo melo <reginaldo.melo56@gmail.com>
2020-07-07 18:47:53 -0300

    deploy

---
+++ public/build/precache-manifest.371a5118eecefb032a34e62081c834f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7376d198faf2ad5a4c63c1acb2c2638d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea63ea1acd5a1d060be0",
+    "url": "/static/css/main.8872e215.chunk.css"
+  },
+  {
+    "revision": "42df52afb27515e8517f",
+    "url": "/static/js/2.135c792c.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/static/js/2.135c792c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea63ea1acd5a1d060be0",
+    "url": "/static/js/main.1be93823.chunk.js"
+  },
+  {
+    "revision": "8f2579ff55bd5aa1997a",
+    "url": "/static/js/runtime-main.87831706.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
