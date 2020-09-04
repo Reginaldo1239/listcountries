eeae34e17450fcc70869c7263ef20a7bb638ca3c
reginaldo melo <reginaldo.melo56@gmail.com>
2020-09-04 13:39:35 -0300

    Updates

---
+++ precache-manifest.e244eca1b06cbf8bbbabbf3efe2ed909.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75ab8d849bf4d411dae4ec17bc8eb8fa",
+    "url": "/listcountries/index.html"
+  },
+  {
+    "revision": "17dcc92d6cedf2b0293c",
+    "url": "/listcountries/static/css/main.3c2890b7.chunk.css"
+  },
+  {
+    "revision": "42df52afb27515e8517f",
+    "url": "/listcountries/static/js/2.135c792c.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/listcountries/static/js/2.135c792c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17dcc92d6cedf2b0293c",
+    "url": "/listcountries/static/js/main.9e9c3c6a.chunk.js"
+  },
+  {
+    "revision": "104dd7434fcb75a8b025",
+    "url": "/listcountries/static/js/runtime-main.ec56af33.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/listcountries/static/media/logo.5d5d9eef.svg"
+  }
+]);
